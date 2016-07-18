b21a090b6ff1a33614c8021f5761eda2d6c41b9d
Bruce Dawson <bruce.dawson@cygnus-software.com>
2016-07-17 19:30:15 -0700

    Tweaks to TestSizes.py and Excel graphs

--- ZeroingArrays_2016/TestSizes.py
+++ ZeroingArrays_2016/TestSizes.py
@@ -35,8 +35,10 @@ os.environ["INCLUDE"] = r"C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC
 
 startpath = os.environ["PATH"]
 
-for options in ["/O1 /Oi /Oy-", "/O2 /Oi /Oy-", "/O2 /Oi"]:
-  for bitness in [32, 64]:
+for bitness in [32, 64]:
+  for options in ["/O1 /Oy-", "/O2 /Oy-", "/O2"]:
+    if bitness == 64:
+      options = options.replace(" /Oy-", "")
     print "%d-bit with %s:" % (bitness, options),
     if not terse:
       print
@@ -47,6 +49,7 @@ for options in ["/O1 /Oi /Oy-", "/O2 /Oi /Oy-", "/O2 /Oi"]:
     total = 0
     count = 0
     last_diff = None
+    total_sizes = [(0,0)]
     for buf_size in range(1,65):
       # Using /O2 instead of /O1 changes the results
       # On 32-bit /O2 buids /Oy- changes the results
@@ -75,19 +78,24 @@ for options in ["/O1 /Oi /Oy-", "/O2 /Oi /Oy-", "/O2 /Oi"]:
             SSE_used = False
         lastline = line
 
+      last_total = total_sizes[-1]
+      total_sizes.append((last_total[0] + size1, last_total[1] + size2))
       if size1 != size2:
-        if not terse:
-          print "%2d: %2d -> %2d: %+3d bytes" % (buf_size, size1, size2, size2 - size1),
-          if SSE_used_for1:
-            print " SSE used for = {0};",
-          if SSE_used_for2:
-            print " SSE used for = {};",
-          print
         last_diff = buf_size
+      if not terse:
+        print "%2d: %2d -> %2d: %+3d bytes" % (buf_size, size1, size2, size2 - size1),
+        print "%4.1f%%" % ((size1 - size2) * 100.0 / size1),
+        if SSE_used_for1:
+          print " SSE used for = {0};",
+        if SSE_used_for2:
+          print " SSE used for = {};",
+        print
       total += size2 - size1
       count += 1
 
-    print "Average saving from 1 to %d is %1.3f bytes" % (last_diff, total * -1.0 / last_diff)
+    print "Average saving from 1 to %d is %1.3f bytes," % (last_diff, total * -1.0 / last_diff),
+    last_diff_totals = total_sizes[last_diff]
+    print "%1.2f%%" % ((last_diff_totals[0] - last_diff_totals[1]) * 100.0 / last_diff_totals[0])
     if not terse:
       print
   if not terse:
