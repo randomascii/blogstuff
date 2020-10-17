9f2add94e0b58d8760a69bc9754933bb95dd12cf
Bruce Dawson <brucedawson@cygnus-software.com>
2020-10-17 15:38:30 -0700

    Add copyright header

--- timer_interval/change_interval.cpp
+++ timer_interval/change_interval.cpp
@@ -1,3 +1,26 @@
+/*
+Copyright 2020 Cygnus Software. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+/*
+This program monitors the effects of the current global timer interrupt
+interval as set by timeBeginPeriod. For most interesting results run this
+program while change_interval.exe is running in another window, and when
+nothing else that sets the timer interrupt interval is running.
+*/
+
 #include <Windows.h>
 #include <stdio.h>
 
