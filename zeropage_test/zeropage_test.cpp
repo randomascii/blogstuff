05138fa6b1fc56d9f69aaa66538398eed2dcb71b
Bruce Dawson <brucedawson@cygnus-software.com>
2022-07-09 16:26:03 -0700

    Add zeropage_test.cpp
    This source file should be compiled with "cl zeropage_test.cpp" to
    create an executable called zeropage_test.exe. When this process is run
    it will run another copy of itself in order to increase the allocation
    load on the system. On Windows 10-based operating systems (including
    Windows Server 2022) on machines with 96+ cores this triggers
    pathological behavior in the System process where the
    MiZeroLargePageThread threads spend most of their time fighting over a
    spin lock instead of zeroing memory.

---
+++ zeropage_test/zeropage_test.cpp
@@ -0,0 +1,44 @@
+/*
+Copyright 2022 Bruce Dawson. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+#include <Windows.h>
+
+constexpr int kNumChildProcesses = 1;
+constexpr int kLoopCount = 800000;
+constexpr size_t kSize = 4096;
+
+int main(int argc, char *argv[]) {
+  if (argc == 1) {
+    // The biggest ratio of CPU time spent in this test program versus in the
+    // system process is when there are two processes, so one child process.
+    for (int i = 0; i < kNumChildProcesses; ++i) {
+      STARTUPINFO startup_info = {};
+      PROCESS_INFORMATION process_info = {};
+      CreateProcess("zeropage_test.exe", "zeropage_test.exe child", nullptr,
+                    nullptr, FALSE, 0, nullptr, nullptr, &startup_info,
+                    &process_info);
+    }
+  }
+
+  for (int i = 0; i < kLoopCount; ++i) {
+    void *p =
+        VirtualAlloc(nullptr, kSize, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+    memset(p, 0, kSize);
+    VirtualFree(p, 0, MEM_RELEASE);
+  }
+  return 0;
+}
+
